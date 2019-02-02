325709caf582d8d638f62ef12669e4c00634e686
Cristiano <cristiano_de_almeida@hotmail.com>
2019-02-02 16:32:54 -0200

    Criada pagina de ofertas (carros sem ipva)

--- client/src/app/components/offers/offers.component.ts
+++ client/src/app/components/offers/offers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataApiService } from 'src/app/services/data-api.service';
+import { CarInterface } from "../../models/car-interface";
 
 @Component({
   selector: 'app-offers',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OffersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataApi: DataApiService) { }
+  private cars: CarInterface;
+
 
   ngOnInit() {
+	  this.getOffers(); 
+  }
+  
+  getOffers(){
+	this.dataApi.getOffers()
+	.subscribe((cars: CarInterface) => (this.cars = cars) );
   }
 
 }
