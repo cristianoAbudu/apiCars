9b1a336c70ec92b1026b0e169b1ef97315e5fbaf
Cristiano <cristiano_de_almeida@hotmail.com>
2019-02-02 00:16:01 -0200

    front inicial

---
+++ client/src/app/components/details-car/details-car.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-car',
+  templateUrl: './details-car.component.html',
+  styleUrls: ['./details-car.component.css']
+})
+export class DetailsCarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
