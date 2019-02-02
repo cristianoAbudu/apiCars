340f6c554378699adf52e1d498fc1cc11e0da169
Cristiano <cristiano_de_almeida@hotmail.com>
2019-02-02 15:40:52 -0200

    Criando Interfaces

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataApiService } from 'src/app/services/data-api.service';
+import { CarInterface } from "../../models/car-interface";
+
 
 @Component({
   selector: 'app-home',
@@ -9,26 +11,16 @@ import { DataApiService } from 'src/app/services/data-api.service';
 export class HomeComponent implements OnInit {
 
   constructor(private dataApi: DataApiService) { }
+  private cars: CarInterface;
+
 
   ngOnInit() {
-	  this.getListCars();
-	  this.getCar('1');
-	  this.getOffers();
+	  this.getListCars(); 
   }
   
   getListCars(){
 	this.dataApi.getAllCars()
-	.subscribe((cars) => console.log(cars) );
+	.subscribe((cars: CarInterface) => (this.cars = cars) );
   }
-    
-  getCar(id: string){
-	this.dataApi.getCarById(id)
-	.subscribe((car) => console.log(car) );
-  }    
-  
-  getOffers(){
-	this.dataApi.getOffers()
-	.subscribe((cars) => console.log(cars) );
-  }      
   
 }
