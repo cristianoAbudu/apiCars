1913e1909a7aa74bbabdeeed391f7750506fb0a5
Cristiano <cristiano_de_almeida@hotmail.com>
2019-02-02 16:30:04 -0200

    Pagina de detalhes

--- client/src/app/components/details-car/details-car.component.ts
+++ client/src/app/components/details-car/details-car.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataApiService } from 'src/app/services/data-api.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { CarInterface } from 'src/app/models/car-interface';
 
 @Component({
   selector: 'app-details-car',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsCarComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private dataApi: DataApiService, private route: ActivatedRoute) { }
+  
+  private car: CarInterface = {
+    modelo: '',
+    fabricante: '',
+    ano: null,
+    preco: ''
+  };
+  
   ngOnInit() {
+    const car_id = this.route.snapshot.params['id'];
+    this.getDetails(car_id);
   }
 
+  getDetails(id: string) {
+    this.dataApi.getCarById(id).subscribe(car => (this.car = car));
+  }
 }
